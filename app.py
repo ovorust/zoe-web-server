e5416fd3cabfa806ad4bcdc5192a0347637e5a7d
afonsonavarini <afonsonavarini@hotmail.com>
2025-02-24 19:50:46 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+import google.generativeai as genai
+from dotenv import load_dotenv
+import os
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})  # Habilita CORS para a origem específica
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY")
+
+
+# Configurar a API do Gemini
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-pro')
+chat = model.start_chat(history=[])
+
+@app.route('/')
+def home():
+    return jsonify(message="Hello from Flask!")
+
+@app.route('/chat', methods=['POST'])
+def chat_route():
+    data = request.json
+    print("Recebido:", data)  # Verificar se os dados estão chegando corretamente
+
+    phrase = data.get('message')
+    if not phrase:
+        print("ERROR: Nenhuma mensagem fornecida")
+        return jsonify(error="No message provided"), 400
+
+    print("Mensagem recebida:", phrase)  # Deve imprimir a mensagem corretamente
+
+    response = chat.send_message(f'{phrase}')
+
+    return jsonify(response=f'[zoe]{response.text}')
+
+@app.route('/clear', methods=['POST'])
+def clear_route():
+    global chat
+    chat = model.start_chat(history=[])
+    print("Histórico de chat limpo")
+    return jsonify(message="Chat history cleared")
+
+if __name__ == '__main__':
+    app.run(debug=True)
